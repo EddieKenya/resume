e66142aacbc29b44b85d83890bcd33d3db1d4e03
Edwin Makhanu <edwinmakhanu630@gmail.com>
2023-05-30 21:00:28 +0300

    url updates

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -131,4 +131,5 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
-CSRF_TRUSTED_ORIGINS = ['https://web-production-5c770.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://web-production-e81e.up.railway.app/']
+
